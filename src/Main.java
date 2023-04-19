ed4b9c202debf8d0fc6a47926bb45094c2d491ab
Pavel <hareharrionh@gmail.com>
2023-04-19 17:31:33 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + total + " рублей");
+    }
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        int min = arr[0], max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            } else if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min +
+                " рублей. Максимальная сумма трат за день составила " + max + " рублей");
+    }
+    public static void task3() {
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        double average = (double) sum / 30;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
+    }
+    public static void task4() {
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        String fullName = "";
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            fullName += reverseFullName[i];
+        }
+        System.out.println(fullName);
     }
 }
